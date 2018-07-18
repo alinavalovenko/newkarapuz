75f52be9daaf08ea0533950de45c71a4ded39081
alinavalovenko <alina.valovenko@gmail.com>
2018-07-18 16:05:24 +0300

    installed advanced woo search, woocommerce, yith wishlist

--- wp-content/themes/mkarapuz/header.php
+++ wp-content/themes/mkarapuz/header.php
@@ -21,7 +21,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'mkarapuz' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Вверх', 'mkarapuz' ); ?></a>
 
     <header id="masthead" class="site-header" role="banner">
 

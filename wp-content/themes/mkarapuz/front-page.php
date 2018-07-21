bcb8dfd2820b86ed9c7f6fcebf2f703bae6b40d3
valovenko <alina.valovenko@gmail.com>
2018-07-21 16:38:13 +0300

    added markup for front page

--- wp-content/themes/mkarapuz/front-page.php
+++ wp-content/themes/mkarapuz/front-page.php
@@ -8,5 +8,62 @@
  */
 
 get_header(); ?>
-<div>Front Page template. </div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="container-fluid">
+            <div class="kz-top-banner-wrap">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8">left banner</div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="kz-top-right-banner">top right banner</div>
+                        <div class="kz-bottom-right-banner">bottom left banner</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="kz-icon-section-wrap">
+                <div class="row">
+                    Icons
+                </div>
+            </div>
+            <div class="kz-categories-banner-wrap">
+                categories-banner-wrap
+            </div>
+            <div class="kz-sales-section-wrap">
+                Sales products
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="kz-new-prosucts-secrion">
+                kz-new-prosucts-secrion
+            </div>
+        </div>
+        <div class="container">
+            <div class="kz-bottom-banners-wrap">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-6">Left Banner</div>
+                    <div class="col-xs-12 col-sm-6 col-md-6">Right Banner</div>
+                </div>
+            </div>
+        </div>
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
 <?php get_footer(); ?>

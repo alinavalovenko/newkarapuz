f1ae2668a0855442ef50f3f9ecbd1b8bf2610bb1
valovenko <alina.valovenko@gmail.com>
2018-07-21 22:00:26 +0300

    added styles to home page

--- wp-content/themes/mkarapuz/front-page.php
+++ wp-content/themes/mkarapuz/front-page.php
@@ -25,34 +25,44 @@ if ( $top_banners['left_big_banner_img'] !== false
 	$kz_right_bottom_banner     = KARAPUZ_IMAGES . '/banners/02_krest.png';
 	$kz_right_bottom_banner_alt = 'Именная крыжма для крещения за 2 дня';
 }
-$icons              = get_field( 'kz_home_icons_repeater' );
-$icons_count        = count( $icons );
-$categories_banners = get_field( 'kz_home_categories_bunner_repeater' );
-$categories_banners_count = count($categories_banners);
-$sales_products = get_field( 'kz_home_sale_section_text' );
-$new_products = get_field( 'kz_home_new_products_section_text' );
-$popular_products = get_field('kz_home_popular_products_section');
-$bottom_banners = get_field('kz_home_bottom_banners_group');
-$sales_page_url = get_field('kz_home_sale_section_url');
-$newest_page_url = get_field('kz_home_new_products_section_text');
-$popular_page_url = get_field('kz_home_popular_products_section_text');
+$icons                    = get_field( 'kz_home_icons_repeater' );
+$icons_count              = count( $icons );
+$categories_banners       = get_field( 'kz_home_categories_bunner_repeater' );
+$categories_banners_count = count( $categories_banners );
+$sales_products           = get_field( 'kz_home_sale_section_text' );
+$new_products             = get_field( 'kz_home_new_products_section_text' );
+$popular_products         = get_field( 'kz_home_popular_products_section' );
+$bottom_banners           = get_field( 'kz_home_bottom_banners_group' );
+$sales_page_url           = get_field( 'kz_home_sale_section_url' );
+$newest_page_url          = get_field( 'kz_home_new_products_section_text' );
+$popular_page_url         = get_field( 'kz_home_popular_products_section_text' );
 
 get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <div class="container-fluid">
-            <div class="kz-top-banner-wrap">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-8">
+        <div class="container-fluid kz-top-banner-wrap">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 kz-banner-holder">
+                    <div class="kz-left-banner-holder">
                         <img src="<?php echo $kz_left_banner; ?>" alt="<?php echo $kz_left_banner_alt; ?>"/>
+                        <div class="kz-banner-slogan">
+							<?php echo $kz_left_banner_alt; ?>
+                        </div>
                     </div>
-                    <div class="col-xs-12 col-sm-4">
-                        <div class="kz-top-right-banner">
-                            <img src="<?php echo $kz_right_top_banner; ?>"
-                                 alt="<?php echo $kz_right_top_banner_alt; ?>"></div>
-                        <div class="kz-bottom-right-banner">
-                            <img src="<?php echo $kz_right_bottom_banner; ?>"
-                                 alt="<?php echo $kz_right_bottom_banner_alt; ?>"/>
+                </div>
+                <div class="col-xs-12 col-sm-4 kz-banner-holder">
+                    <div class="kz-top-right-banner">
+                        <img src="<?php echo $kz_right_top_banner; ?>"
+                             alt="<?php echo $kz_right_top_banner_alt; ?>">
+                    <div class="kz-banner-slogan">
+						<?php echo $kz_right_top_banner_alt; ?>
+                    </div>
+                    </div>
+                    <div class="kz-bottom-right-banner">
+                        <img src="<?php echo $kz_right_bottom_banner; ?>"
+                             alt="<?php echo $kz_right_bottom_banner_alt; ?>"/>
+                        <div class="kz-banner-slogan">
+							<?php echo $kz_right_bottom_banner_alt; ?>
                         </div>
                     </div>
                 </div>
@@ -93,52 +103,58 @@ get_header(); ?>
             </div>
             <div class="kz-categories-banner-wrap">
                 <div class="row">
-	                <?php for ( $i = 0; $i < $categories_banners_count; $i ++ ): ?>
+					<?php for ( $i = 0; $i < $categories_banners_count; $i ++ ): ?>
                         <div class="col-xs-12 col-sm-6">
                             <div class="kz-categories-banner">
-                                <a href="<?php echo (empty($categories_banners[$i]['kz_home_category_link_text'])) ? '#' : $categories_banners[$i]['kz_home_category_link_text'];?>">
+                                <a href="<?php echo ( empty( $categories_banners[ $i ]['kz_home_category_link_text'] ) ) ? '#' : $categories_banners[ $i ]['kz_home_category_link_text']; ?>">
                                     <div class="category-banner-holder">
-                                        <img src="<?php echo (empty($categories_banners[$i]['home_category_banner_img']['url'])) ? KARAPUZ_IMAGES . '/banners/cat-banner.png': $categories_banners[$i]['home_category_banner_img']['url'] ;?>" alt="<?php echo $categories_banners[$i]['kz_category_name_text'];?>">
-                                        <span><?php echo $categories_banners[$i]['kz_category_name_text'];?></span>
+                                        <img src="<?php echo ( empty( $categories_banners[ $i ]['home_category_banner_img']['url'] ) ) ? KARAPUZ_IMAGES . '/banners/cat-banner.png' : $categories_banners[ $i ]['home_category_banner_img']['url']; ?>"
+                                             alt="<?php echo $categories_banners[ $i ]['kz_category_name_text']; ?>">
+                                        <span><?php echo $categories_banners[ $i ]['kz_category_name_text']; ?></span>
                                     </div>
                                 </a>
                             </div>
                         </div>
-	                <?php endfor; ?>
+					<?php endfor; ?>
                 </div>
             </div>
             <div class="kz-sales-section-wrap">
                 <div class="kz-sales-section-title">
                     <h3>Скидки</h3>
-                    <a href="<? echo (empty($sales_page_url)) ? '#' : $sales_page_url ;?>">Посмотреть весь ассортимент</a>
+                    <a href="<? echo ( empty( $sales_page_url ) ) ? '#' : $sales_page_url; ?>">Посмотреть весь
+                        ассортимент</a>
                 </div>
-                <?php echo do_shortcode($sales_products); ?>
+				<?php echo do_shortcode( $sales_products ); ?>
             </div>
         </div>
-        <div class="container-fluid">
+        <div class="container-fluid kz-newest-products-wrap">
             <div class="kz-newest-products-section">
-                <div class="kz-newest-section-title">
+                <div class="container kz-newest-section-title">
                     <h3>Новинки</h3>
-                    <a href="<? echo (empty($newest_page_url)) ? '#' : $newest_page_url ;?>">Посмотреть весь ассортимент</a>
+                    <a href="<? echo ( empty( $newest_page_url ) ) ? '#' : $newest_page_url; ?>">Посмотреть весь
+                        ассортимент</a>
                 </div>
-	            <?php echo do_shortcode($new_products); ?>
+				<?php echo do_shortcode( $new_products ); ?>
             </div>
         </div>
         <div class="container">
             <div class="kz-popular-products-wrap">
                 <div class="kz-popular-section-title">
                     <h3>Популярное</h3>
-                    <a href="<? echo (empty($popular_page_url)) ? '#' : $popular_page_url ;?>">Посмотреть весь ассортимент</a>
+                    <a href="<? echo ( empty( $popular_page_url ) ) ? '#' : $popular_page_url; ?>">Посмотреть весь
+                        ассортимент</a>
                 </div>
-                <?php echo do_shortcode($popular_products); ?>
+				<?php echo do_shortcode( $popular_products ); ?>
             </div>
             <div class="kz-bottom-banners-wrap">
                 <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-6">
-                        <img src="<?php echo (empty($bottom_banners['kz_home_left_banner_img']))? KARAPUZ_IMAGES . '/banners/cat-bottom-banner.jpg':$bottom_banners['kz_home_left_banner_img'] ;?>" alt="bottom left banner">
+                        <img src="<?php echo ( empty( $bottom_banners['kz_home_left_banner_img'] ) ) ? KARAPUZ_IMAGES . '/banners/cat-bottom-banner.jpg' : $bottom_banners['kz_home_left_banner_img']; ?>"
+                             alt="bottom left banner">
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6">
-                        <img src="<?php echo (empty($bottom_banners['kz_home_right_banner_img']))? KARAPUZ_IMAGES . '/banners/cat-bottom-banner.jpg':$bottom_banners['kz_home_left_banner_img'] ;?>" alt="bottom right banner">
+                        <img src="<?php echo ( empty( $bottom_banners['kz_home_right_banner_img'] ) ) ? KARAPUZ_IMAGES . '/banners/cat-bottom-banner.jpg' : $bottom_banners['kz_home_left_banner_img']; ?>"
+                             alt="bottom right banner">
                     </div>
                 </div>
             </div>

471c0f137de3a62757ddda9e57f37802d15071ea
alinavalovenko <alina.valovenko@gmail.com>
2018-07-23 13:08:39 +0300

    moved woocommerce template to the  theme

--- wp-content/themes/mkarapuz/functions.php
+++ wp-content/themes/mkarapuz/functions.php
@@ -80,15 +80,3 @@
 		echo wc_get_template( 'cart/mini-cart.php' );
 		die();
 	}
-	add_filter( 'wp_ajax_nopriv_mode_theme_update_mini_cart', 'kz_theme_update_mini_cart' );
-	add_filter( 'wp_ajax_mode_theme_update_mini_cart', 'kz_theme_update_mini_cart' );
-
-	if( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_ajax_update_count' ) ){
-		function yith_wcwl_ajax_update_count(){
-			wp_send_json( array(
-				'count' => yith_wcwl_count_all_products()
-			) );
-		}
-		add_action( 'wp_ajax_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-		add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-	}

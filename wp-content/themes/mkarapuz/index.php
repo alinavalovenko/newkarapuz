a86dc40d6c2dd46dc3f86a28cb396c5327129fc9
alinavalovenko <alina.valovenko@gmail.com>
2018-07-24 19:46:04 +0300

    added default markup for page, single and index template

--- wp-content/themes/mkarapuz/index.php
+++ wp-content/themes/mkarapuz/index.php
@@ -1,20 +1,42 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @package sgo-2018
- * @since 1.0
- * @version 1.0
- */
+	/**
+	 * The main template file
+	 *
+	 * This is the most generic template file in a WordPress theme
+	 * and one of the two required files for a theme (the other being style.css).
+	 * It is used to display a page when nothing more specific matches a query.
+	 * E.g., it puts together the home page when no home.php file exists.
+	 *
+	 * @package sgo-2018
+	 * @since 1.0
+	 * @version 1.0
+	 */
 
-get_header(); ?>
-	<div class="container">
+	get_header(); ?>
+    <div class="wrap container">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-	</div>
+				<?php
+					if ( have_posts() ) :
+
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							the_content();
+
+						endwhile;
+					else :
+
+						get_template_part( 'template-parts/post/content', 'none' );
+
+					endif;
+				?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+		<?php get_sidebar(); ?>
+    </div><!-- .wrap -->
 
 <?php get_footer(); ?>
--- wp-content/themes/mkarapuz/page.php
+++ wp-content/themes/mkarapuz/page.php
@@ -13,5 +13,29 @@
  */
 
 get_header(); ?>
+<div class="wrap container">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
+			<?php
+				if ( have_posts() ) :
+
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						the_content();
+
+					endwhile;
+				else :
+
+					get_template_part( 'template-parts/post/content', 'none' );
+
+				endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 <?php get_footer(); ?>

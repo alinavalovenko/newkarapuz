65ccbb968d1c101dbdc303f1dde5a17befaecd75
alinavalovenko <alina.valovenko@gmail.com>
2018-07-24 13:33:51 +0300

    added styles for breadcrumbs

--- wp-content/themes/mkarapuz/functions.php
+++ wp-content/themes/mkarapuz/functions.php
@@ -192,6 +192,6 @@
 	add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_delimiter' );
 	function wcc_change_breadcrumb_delimiter( $defaults ) {
 		// Change the breadcrumb delimeter from '/' to '>'
-		$defaults['delimiter'] = ' &gt; ';
+		$defaults['delimiter'] = '<span class="kz-breadcrumbs-delimeter"> &gt; </span>';
 		return $defaults;
 	}
